6a7d03b7d60969f73b2fa1c4312bcb1d09c6af93
Guillaume Tucker <guillaume.tucker@plasticlogic.com>
2014-02-07 15:45:46 +0000

    setver.sh: also update project version in Doxyfile

--- setver.sh
+++ setver.sh
@@ -4,8 +4,10 @@ set -e
 
 ver="$1"
 
-sed -i s/"\(VERSION\[\] = \"\)\(.*\)\(\";\)"/"\1v$ver\3"/ main.c
-sed -i s/"^\(:Revision: \)\(.*\)\$"/"\1v$ver"/ Documentation/index.rst
-git add main.c Documentation/index.rst
+sed -i -e s/"\(VERSION\[\] = \"\)\(.*\)\(\";\)"/"\1v$ver\3"/ main.c
+sed -i -e s/"^\(:Revision: \)\(.*\)\$"/"\1v$ver"/ Documentation/index.rst
+sed -i -e s/"\(PROJECT_NUMBER.* =\) \"\(.*\)\""/"\1 \"v$ver\""/ Doxyfile
+unix2dos main.c Documentation/index.rst Doxyfile
+git add main.c Documentation/index.rst Doxyfile
 
 exit 0
