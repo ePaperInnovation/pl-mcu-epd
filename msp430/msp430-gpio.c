0c5ec073334444ea82420140aed033d055a77293
Guillaume Tucker <guillaume.tucker@plasticlogic.com>
2014-02-11 12:22:14 +0000

    msp430-gpio: use new pl_gpio API + tidy-up

--- msp430/msp430-gpio.c
+++ msp430/msp430-gpio.c
@@ -1,7 +1,7 @@
 /*
   Plastic Logic EPD project on MSP430
 
-  Copyright (C) 2013 Plastic Logic Limited
+  Copyright (C) 2013, 2014 Plastic Logic Limited
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -17,9 +17,11 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 /*
- * msp430-gpio.c -- MSP430 gpio pin management functions
+ * msp430-gpio.c -- MSP430 GPIO pin management functions
  *
- * Authors: Nick Terry <nick.terry@plasticlogic.com>
+ * Authors:
+ *   Nick Terry <nick.terry@plasticlogic.com>
+ *   Guillaume Tucker <guillaume.tucker@plasticlogic.com?
  *
  * The purpose of this module is to allow some dynamic control of GPIO
  * configuration. It is not intended to validate all requests completely
@@ -31,21 +33,28 @@
  *
  */
 
+#include <pl/gpio.h>
 #include <msp430.h>
-#include <stddef.h>
-#include "types.h"
+#include <stdint.h>
+#include <stdlib.h>
 #include "assert.h"
 #include "msp430-gpio.h"
 
-#define GPIO_PORT(gpio)		(((gpio) >> 8) & 0x0FF)
-#define GPIO_PIN(gpio)		((gpio) & 0x0FF)
+#define LOG_TAG "msp430-gpio"
+#include "utils.h"
 
-#define GPIO_CHECK_PARAMETERS	1
-#define GPIO_DEBUG				0
-#define	GPIO_ALLOC_FAIL_FATAL	1
+#define GPIO_PORT(_gpio) (((_gpio) >> 8) & 0xFF)
+#define GPIO_PIN(_gpio) ((_gpio) & 0xFF)
 
-// nasty token pasting stuff that allows us to parameterise the
-// access to registers we need
+/* Set to 1 to enable extra GPIO debug messages */
+#define GPIO_DEBUG 0
+
+/* Set to 1 to enable checks (takes a bit more code space) */
+#define GPIO_CHECK_PARAMETERS 1
+
+/* Set to 1 to enable all get-set checks (slows down I/O) */
+#define GPIO_CHECK_GET_SET 1
+
 #define PxIN(_x_)	P ##_x_ ##IN
 #define PxOUT(_x_)	P ##_x_ ##OUT
 #define PxDIR(_x_)	P ##_x_ ##DIR
@@ -56,295 +65,204 @@
 #define PxIE(_x_)	P ##_x_ ##IE
 #define PxIFG(_x_)	P ##_x_ ##IFG
 
-/* could maybe store offsets if we can compute them?
- * This is a big table but its in flash. */
+/* Could maybe not store offsets if we can compute them?
+ * This is a big table but it's in flash. */
 static const struct io_config {
-	volatile u8 *in;		// if NULL then this port not present
-	volatile u8 *out;
-	volatile u8 *dir;
-	volatile u8 *resenable;
-	volatile u8 *strength;
-	volatile u8 *fnselect;
-	volatile u8 *edge;		// only in ports 1 & 2
-	volatile u8 *intenable;	// only in ports 1 & 2
-	volatile u8 *intflag;	// only in ports 1 & 2
-} gpio_defs[] = {
+	volatile uint8_t *in; /* if NULL then this port not present */
+	volatile uint8_t *out;
+	volatile uint8_t *dir;
+	volatile uint8_t *resenable;
+	volatile uint8_t *strength;
+	volatile uint8_t *fnselect;
+	volatile uint8_t *edge;         /* only in ports 1 & 2 */
+	volatile uint8_t *intenable;    /* only in ports 1 & 2 */
+	volatile uint8_t *intflag;      /* only in ports 1 & 2 */
+} msp430_gpio_defs[] = {
 #ifdef __MSP430_HAS_PORT1_R__
-		{ &PxIN(1),	&PxOUT(1),  &PxDIR(1),  &PxREN(1),  &PxDS(1),  &PxSEL(1),  &PxIES(1),  &PxIE(1),  &PxIFG(1) },
+		{ &PxIN(1), &PxOUT(1), &PxDIR(1), &PxREN(1), &PxDS(1),
+		  &PxSEL(1), &PxIES(1), &PxIE(1), &PxIFG(1) },
 #else
 		{ NULL },
 #endif
 #ifdef __MSP430_HAS_PORT2_R__
-		{ &PxIN(2),	&PxOUT(2),  &PxDIR(2),  &PxREN(2),  &PxDS(2),  &PxSEL(2),  &PxIES(2),  &PxIE(2),  &PxIFG(2) },
+		{ &PxIN(2), &PxOUT(2), &PxDIR(2), &PxREN(2), &PxDS(2),
+		  &PxSEL(2), &PxIES(2), &PxIE(2), &PxIFG(2) },
 #else
 		{ NULL },
 #endif
 #ifdef __MSP430_HAS_PORT3_R__
-		{ &PxIN(3),	&PxOUT(3),  &PxDIR(3),  &PxREN(3),  &PxDS(3),  &PxSEL(3),  NULL,  NULL,  NULL },
+		{ &PxIN(3), &PxOUT(3), &PxDIR(3), &PxREN(3), &PxDS(3),
+		  &PxSEL(3), NULL, NULL, NULL },
 #else
 		{ NULL },
 #endif
 #ifdef __MSP430_HAS_PORT4_R__
-		{ &PxIN(4),	&PxOUT(4),  &PxDIR(4),  &PxREN(4),  &PxDS(4),  &PxSEL(4),  NULL,  NULL,  NULL },
+		{ &PxIN(4), &PxOUT(4), &PxDIR(4), &PxREN(4), &PxDS(4),
+		  &PxSEL(4), NULL, NULL, NULL },
 #else
 		{ NULL },
 #endif
 #ifdef __MSP430_HAS_PORT5_R__
-		{ &PxIN(5),	&PxOUT(5),  &PxDIR(5),  &PxREN(5),  &PxDS(5),  &PxSEL(5),  NULL,  NULL,  NULL },
+		{ &PxIN(5), &PxOUT(5), &PxDIR(5), &PxREN(5), &PxDS(5),
+		  &PxSEL(5), NULL, NULL, NULL },
 #else
 		{ NULL },
 #endif
 #ifdef __MSP430_HAS_PORT6_R__
-		{ &PxIN(6),	&PxOUT(6),  &PxDIR(6),  &PxREN(6),  &PxDS(6),  &PxSEL(6),  NULL,  NULL,  NULL },
+		{ &PxIN(6), &PxOUT(6), &PxDIR(6), &PxREN(6), &PxDS(6),
+		  &PxSEL(6), NULL, NULL, NULL },
 #else
 		{ NULL },
 #endif
 #ifdef __MSP430_HAS_PORT7_R__
-		{ &PxIN(7),	&PxOUT(7),  &PxDIR(7),  &PxREN(7),  &PxDS(7),  &PxSEL(7),  NULL,  NULL,  NULL },
+		{ &PxIN(7), &PxOUT(7), &PxDIR(7), &PxREN(7), &PxDS(7),
+		  &PxSEL(7), NULL, NULL, NULL },
 #else
 		{ NULL },
 #endif
 #ifdef __MSP430_HAS_PORT8_R__
-		{ &PxIN(8),	&PxOUT(8),  &PxDIR(8),  &PxREN(8),  &PxDS(8),  &PxSEL(8),  NULL,  NULL,  NULL },
+		{ &PxIN(8), &PxOUT(8), &PxDIR(8), &PxREN(8), &PxDS(8),
+		  &PxSEL(8), NULL, NULL, NULL },
 #else
 		{ NULL },
 #endif
 #ifdef __MSP430_HAS_PORT9_R__
-		{ &PxIN(9),	&PxOUT(9),  &PxDIR(9),  &PxREN(9),  &PxDS(9),  &PxSEL(9),  NULL,  NULL,  NULL },
+		{ &PxIN(9), &PxOUT(9), &PxDIR(9), &PxREN(9), &PxDS(9),
+		  &PxSEL(9), NULL, NULL, NULL },
 #else
 		{ NULL },
 #endif
 #ifdef __MSP430_HAS_PORT10_R__
-		{ &PxIN(10),&PxOUT(10), &PxDIR(10), &PxREN(10), &PxDS(10), &PxSEL(10), NULL, NULL, NULL },
+		{ &PxIN(10),&PxOUT(10), &PxDIR(10), &PxREN(10), &PxDS(10),
+		  &PxSEL(10), NULL, NULL, NULL },
 #else
 		{ NULL },
 #endif
 #ifdef __MSP430_HAS_PORT11_R__
-		{ &PxIN(11),&PxOUT(11), &PxDIR(11), &PxREN(11), &PxDS(11), &PxSEL(11), NULL, NULL, NULL },
+		{ &PxIN(11),&PxOUT(11), &PxDIR(11), &PxREN(11), &PxDS(11),
+		  &PxSEL(11), NULL, NULL, NULL },
 #else
 		{ NULL },
 #endif
 };
 
-// debug use to track allocations of gpios
-static u8 alloc_map[ARRAY_SIZE(gpio_defs)];
-
-static void gpio_mark(u16 port, u16 pinmask, int claim)
-{
-	if (claim)
-		alloc_map[port] |= pinmask;
-	else
-		alloc_map[port] &= ~pinmask;
-}
-
-void gpio_mark_busy(int gpio)
-{
-	gpio_mark(GPIO_PORT(gpio), GPIO_PIN(gpio), true);
-}
-
-static int gpio_check(u16 port, u16 pinmask)
-{
-	if (port >= ARRAY_SIZE(gpio_defs))
-		return -ENXIO;
-
-	if (gpio_defs[port].in == NULL)
-		return -ENXIO;
-
-	if (alloc_map[port] & pinmask)
-		return -EBUSY;
-
-	return 0;
-}
-
-#ifndef NDEBUG
-static int gpio_is_allocated(u16 port, u16 pinmask)
-{
-	return (alloc_map[port] & pinmask);
-}
-#endif
-
 #if GPIO_DEBUG
-static int gpio_pin_number(u16 pinmask)
+static int msp430_gpio_pin_number(uint16_t pinmask)
 {
-	int bit = 0;
-	while (pinmask >>= 1)
-	{
-		bit++;
-	}
+	int bit;
+
+	for (bit = 0; pinmask; pinmask >>= 1, bit++);
+
 	return bit;
 }
 #endif
 
-void gpio_init(void)
+static void msp430_gpio_check_port(uint16_t port)
 {
+	if (port >= ARRAY_SIZE(msp430_gpio_defs))
+		abort_msg("Invalid port number");
+
+	if (msp430_gpio_defs[port].in == NULL)
+		abort_msg("Port not available");
 }
 
-#if GPIO_CHECK_PARAMETERS
-static /*inline*/ int all_set(int flags, int bits)
+static int msp430_gpio_config(unsigned gpio, uint16_t flags)
 {
-	return ((flags & bits) == bits);
-}
-
-static /*inline*/ int any_set(int flags, int bits)
-{
-	return ((flags & bits) != 0);
-}
-#endif
-
-int gpio_request(int gpio, u16 flags)
-{
-	int ret;
 	const struct io_config *io;
-	u16 port;
-	u16 pinmask;
-
-	port    = GPIO_PORT(gpio);
-	pinmask = GPIO_PIN(gpio);
+	const uint16_t port = GPIO_PORT(gpio);
+	const uint16_t pinmask = GPIO_PIN(gpio);
 
 #if GPIO_DEBUG
-	printk("gpio_request(%02d:%02d - 0x%04x)\n", (port+1), gpio_pin_number(pinmask), flags);
+	LOG("config gpio=0x%04X (%d.%d)", gpio, (port + 1),
+	    msp430_gpio_pin_number(pinmask));
+	pl_gpio_log_flags(flags);
 #endif
-	ret = gpio_check(port, pinmask);
 
-	if ((ret == -EBUSY) && (flags & PIN_REDEFINE))
-		ret = 0;
+	msp430_gpio_check_port(port);
+	io = &msp430_gpio_defs[port];
 
-	if (ret < 0)
-		goto err_alloc;
-
-	io = &gpio_defs[port];
-
-	/* Validate the request as best we can */
 #if GPIO_CHECK_PARAMETERS
-	/* must be one or the other */
-	if (!any_set(flags, PIN_GPIO | PIN_SPECIAL))
-		goto err_config;
-
-	/* but cannot be a gpio AND a special purpose pin */
-	if (all_set(flags, PIN_GPIO | PIN_SPECIAL))
-		goto err_config;
-
-	/* cannot be an input AND an output */
-	if (all_set(flags, PIN_INPUT | PIN_OUTPUT))
-		goto err_config;
-
-	/* cannot initialise high AND low */
-	if (all_set(flags, PIN_INIT_HIGH | PIN_INIT_LOW))
-		goto err_config;
-
-	/* if flags request interrupts and the port does not support them error */
-	if (all_set(flags, PIN_INTERRUPT) && !io->intflag)
-		goto err_config;
-
-	/* cannot apply pull up parameters to output pins */
-	if (all_set(flags, PIN_OUTPUT) && any_set(flags, PIN_PULL_UP | PIN_PULL_DOWN))
-		goto err_config;
-
-	/* cannot apply output parameters to input pins */
-	if (all_set(flags, PIN_INPUT) &&
-		any_set(flags, PIN_FULL_DRIVE | PIN_REDUCED_DRIVE | PIN_INIT_HIGH | PIN_INIT_LOW))
-		goto err_config;
+	if (pl_gpio_check_flags(flags))
+		return -1;
 #endif
 
-	/* Now apply the parameters to the io port */
-	if (flags & PIN_GPIO)
-		*io->fnselect &= ~pinmask;
-	else
+	if (flags & PL_GPIO_SPECIAL)
 		*io->fnselect |= pinmask;
+	else
+		*io->fnselect &= ~pinmask;
 
-	if (flags & PIN_OUTPUT)
-	{
+	if (flags & PL_GPIO_OUTPUT) {
 		*io->dir |= pinmask;
-		if (flags & PIN_INIT_HIGH)
+
+		if (flags & PL_GPIO_INIT_H)
 			*io->out |= pinmask;
 		else
 			*io->out &= ~pinmask;
 
-		if (flags & PIN_REDUCED_DRIVE)
+		if (flags & PL_GPIO_DRIVE_REDUCED)
 			*io->strength &= ~pinmask;
 		else
 			*io->strength |= pinmask;
-	}
-
-	if (flags & PIN_INPUT)
-	{
+	} else if (flags & PL_GPIO_INPUT) {
 		*io->dir &= ~pinmask;
 		*io->resenable &= ~pinmask;
-		if (any_set(flags, PIN_PULL_UP | PIN_PULL_DOWN))
-		{
+
+		if (flags & (PL_GPIO_PU | PL_GPIO_PD)) {
 			*io->resenable |= pinmask;
-			if (flags & PIN_PULL_UP)
+
+			if (flags & PL_GPIO_PU)
 				*io->out |= pinmask;
 			else
 				*io->out &= ~pinmask;
 		}
 
-		*io->intenable &= ~pinmask;
-		if (flags & PIN_INTERRUPT)
-		{
+		if (flags & PL_GPIO_INTERRUPT) {
 			*io->intenable |= pinmask;
-			if (flags & PIN_RISING_EDGE)
+
+			if (flags & PL_GPIO_INT_RISE)
 				*io->edge &= ~pinmask;
 			else
 				*io->edge |= pinmask;
+		} else {
+			*io->intenable &= ~pinmask;
 		}
 	}
 
-	gpio_mark(port, pinmask, 1);
-
 	return 0;
+}
 
-err_config:
-	ret = -EPERM;
-err_alloc:
+static int msp430_gpio_get(unsigned gpio)
+{
+	const uint16_t port = GPIO_PORT(gpio);
+	const uint16_t pinmask = GPIO_PIN(gpio);
 
-#if GPIO_ALLOC_FAIL_FATAL
-	assert(ret == 0);
+#if GPIO_CHECK_GET_SET
+	msp430_gpio_check_port(port);
 #endif
 
-	return ret;
+	return *msp430_gpio_defs[port].in & pinmask;
 }
 
-void gpio_set_value(int gpio, int state)
+static void msp430_gpio_set(unsigned gpio, int value)
 {
-	int port    = GPIO_PORT(gpio);
-	int pinmask = GPIO_PIN(gpio);
+	const uint16_t port = GPIO_PORT(gpio);
+	const uint16_t pinmask = GPIO_PIN(gpio);
 
-	assert(gpio_is_allocated(port, pinmask));
+#if GPIO_CHECK_GET_SET
+	msp430_gpio_check_port(port);
+#endif
 
-	if (state)
-		*gpio_defs[port].out |= pinmask;
+	if (value)
+		*msp430_gpio_defs[port].out |= pinmask;
 	else
-		*gpio_defs[port].out &= ~pinmask;
+		*msp430_gpio_defs[port].out &= ~pinmask;
 }
 
-void gpio_set_value_hi(int gpio)
+int msp430_gpio_init(struct pl_gpio *gpio)
 {
-	int port    = GPIO_PORT(gpio);
-	int pinmask = GPIO_PIN(gpio);
+	gpio->config = msp430_gpio_config;
+	gpio->get = msp430_gpio_get;
+	gpio->set = msp430_gpio_set;
 
-	assert(gpio_is_allocated(port, pinmask));
-
-	*gpio_defs[port].out |= pinmask;
+	return 0;
 }
-
-void gpio_set_value_lo(int gpio)
-{
-	int port    = GPIO_PORT(gpio);
-	int pinmask = GPIO_PIN(gpio);
-
-	assert(gpio_is_allocated(port, pinmask));
-
-	*gpio_defs[port].out &= ~pinmask;
-}
-
-int gpio_get_value(int gpio)
-{
-	int port    = GPIO_PORT(gpio);
-	int pinmask = GPIO_PIN(gpio);
-
-	assert(gpio_is_allocated(port, pinmask));
-
-	return *gpio_defs[port].in & pinmask;
-}
-
--- msp430/msp430-gpio.h
+++ msp430/msp430-gpio.h
@@ -1,7 +1,7 @@
 /*
   Plastic Logic EPD project on MSP430
 
-  Copyright (C) 2013 Plastic Logic Limited
+  Copyright (C) 2013, 2014 Plastic Logic Limited
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -19,40 +19,19 @@
 /*
  * msp430-gpio.h -- MSP430 gpio pin management functions
  *
- * Authors: Nick Terry <nick.terry@plasticlogic.com>
+ * Authors:
+ *   Nick Terry <nick.terry@plasticlogic.com>
+ *   Guillaume Tucker <guillaume.tucker@plasticlogic.com>
  *
  */
 
-#ifndef MSP430_GPIO_H_
-#define MSP430_GPIO_H_
+#ifndef MSP430_GPIO_H
+#define MSP430_GPIO_H 1
 
-#include <msp430.h>
+#define MSP430_GPIO(_port, _pin) (((_port) - 1) << 8 | (1 << (_pin)))
 
-enum PinDefs {
-	PIN_GPIO = BIT0,			// pin is GPIO pin
-	PIN_SPECIAL = BIT1,			// pin is special purpose (eg SPI)
-	PIN_OUTPUT = BIT2,			// pin is output
-		PIN_FULL_DRIVE = BIT3,		// pin (output) full drive strength
-	  	PIN_REDUCED_DRIVE = BIT4,	// pin (output) reduced drive strength
-	  	PIN_INIT_HIGH = BITB,		// pin (output) initialise high
-	  	PIN_INIT_LOW = BITC,		// pin (output) initialise low
-	PIN_INPUT = BIT5,			// pin is input
-		PIN_PULL_UP = BIT6,			// pin (input) is pulled up
-		PIN_PULL_DOWN = BIT7,		// pin (input) is pulled down
-		PIN_FALLING_EDGE = BIT8,	// pin (input) interrupt on falling edge
-		PIN_RISING_EDGE = BIT9,		// pin (input) interrupt on rising edge
-		PIN_INTERRUPT = BITA,		// pin (input) enable interrupt
-		PIN_REDEFINE = BITD		// redefine existing pin
-};
+struct pl_gpio;
 
-#define GPIO(port, pin)	(((port)-1) << 8 | (1 << (pin)))
+extern int msp430_gpio_init(struct pl_gpio *gpio);
 
-void gpio_init(void);
-void gpio_mark_busy(int gpio);
-int gpio_request(int gpio, u16 flags);
-void gpio_set_value(int gpio, int state);
-int gpio_get_value(int gpio);
-void gpio_set_value_hi(int gpio);
-void gpio_set_value_lo(int gpio);
-
-#endif /* MSP430_GPIO_H_ */
+#endif /* MSP430_GPIO_H */
