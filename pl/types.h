32f93e773a9c6f1c304c1acc3217e90d412df20f
Guillaume Tucker <guillaume.tucker@plasticlogic.com>
2014-02-13 16:37:02 +0000

    add pl/types.h with PL specific types

---
+++ pl/types.h
@@ -0,0 +1,38 @@
+/*
+  Plastic Logic EPD project on MSP430
+
+  Copyright (C) 2013, 2014 Plastic Logic Limited
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
+ * types.h -- General type and constant definitions
+ *
+ * Authors:
+ *   Nick Terry <nick.terry@plasticlogic.com>
+ *   Guillaume Tucker <guillaume.tucker@plasticlogic.com>
+ *
+ */
+
+#ifndef INCLUDE_PL_TYPES_H
+#define INCLUDE_PL_TYPES_H 1
+
+struct pl_area {
+	int left;
+	int top;
+	int width;
+	int height;
+};
+
+#endif /* INCLUDE_PL_TYPES_H */
