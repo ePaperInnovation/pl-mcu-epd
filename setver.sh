8b8105655f474f195ea175a9013bb63b23a9161d
Dirk Bandelow <dirk.bandelow@plasticlogic.com>
2014-04-15 14:13:36 +0200

    setver.h: add date generation

--- setver.sh
+++ setver.sh
@@ -3,9 +3,11 @@
 set -e
 
 ver="$1"
+date=`date +"%d %B %Y"`
 
 sed -i -e s/"\(VERSION\[\] = \"\)\(.*\)\(\";\)"/"\1v$ver\3"/ main.c
 sed -i -e s/"^\(:Revision: \)\(.*\)\$"/"\1v$ver"/ Documentation/index.rst
+sed -i -e s/"^\(:Date: \)\(.*\)\$"/"\1$date"/ Documentation/index.rst
 sed -i -e s/"\(PROJECT_NUMBER.* =\) \"\(.*\)\""/"\1 \"v$ver\""/ Doxyfile
 unix2dos main.c Documentation/index.rst Doxyfile
 git add main.c Documentation/index.rst Doxyfile
